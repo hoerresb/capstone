fc7ad7fd32d23a9fcccbe24babadf39434e933c3
griffinp <griffinp@uwm.edu>
2014-04-02 19:13:25 -0500

    Added method definitions to Factory.java for reference during UI programming.  More may be necessary depending on functional requirements. Added setters/getters/flags to Activity.java.

--- GymTrack/src/edu/gymtrack/db/Factory.java
+++ GymTrack/src/edu/gymtrack/db/Factory.java
@@ -1,9 +1,17 @@
 package edu.gymtrack.db;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Map;
 
 import edu.gymtrack.controller.Authentication;
+import edu.gymtrack.model.Activity;
+import edu.gymtrack.model.Equipment;
+import edu.gymtrack.model.EquipmentType;
+import edu.gymtrack.model.PlanElement;
+import edu.gymtrack.model.User;
+import edu.gymtrack.model.WorkoutLog;
+import edu.gymtrack.model.WorkoutPlan;
 
 public class Factory {
 	public static Authentication createAuthentication() throws SQLException{
@@ -11,4 +19,84 @@ public class Factory {
 		Map<String, String> pairs = db.getAuthPairs();
 		return new Authentication(pairs);
 	}
+	
+	// methods to pull data from the database
+	
+	public ArrayList<Activity> getActivities()
+	{
+	
+	}
+	
+	public ArrayList<Equipment> getEquipment()
+	{
+	
+	}
+	
+	public ArrayList<EquipmentType> getEquipmentTypes()
+	{
+	
+	}
+	
+	public ArrayList<PlanElement> getPlanElements(WorkoutPlan plan)
+	{
+	
+	}
+	
+	public ArrayList<User> getUsers()
+	{
+	
+	}
+	
+	public ArrayList<WorkoutLog> getWorkoutLogs(User user)
+	{
+		
+	}
+	
+	public ArrayList<WorkoutPlan> getWorkoutPlansForUser(User user)
+	{
+		
+	}
+	
+	public ArrayList<WorkoutPlan> getWorkoutPlans()
+	{
+		
+	}
+	
+	// methods to update data in the database
+	
+	public void updateActivities(ArrayList<Activity> newdata)
+	{
+		
+	}
+	
+	public void updateEquipment(ArrayList<Equipment> newdata)
+	{
+		
+	}
+	
+	public void updateEquipmentType(ArrayList<EquipmentType> newdata)
+	{
+		
+	}
+	
+	public void updatePlanElements(ArrayList<PlanElement> newdata)
+	{
+		
+	}
+	
+	public void updateUsers(ArrayList<User> newdata)
+	{
+		
+	}
+	
+	public void updateWorkoutLogs(ArrayList<WorkoutLog> newdata)
+	{
+		
+	}
+	
+	public void updateWorkoutPlans(ArrayList<WorkoutPlan> newdata)
+	{
+		
+	}
+	
 }
--- GymTrack/src/edu/gymtrack/model/Activity.java
+++ GymTrack/src/edu/gymtrack/model/Activity.java
@@ -2,13 +2,44 @@ package edu.gymtrack.model;
 
 public class Activity {
 
+	private int key;
 	private String name;
+	private boolean newFlag;
+	private boolean editFlag;
+	private boolean deleteFlag;
 	
-	public Activity(String name){
+	
+	public Activity(int key, String name, boolean newEntry){
+		this.key = key;
 		this.name = name;
+		if (newEntry)
+			newFlag = true;
+	}
+	
+	// getters
+	
+	public int getKey(){
+		return this.key;
 	}
 	
 	public String getName(){
 		return this.name;
 	}
+	
+	// setters
+	
+	public void setKey(int key){
+		this.key = key;
+	    this.editFlag = true;
+	}
+	
+	public void setName(String name){
+		this.name = name;
+		this.editFlag = true;
+	}
+	
+	public void delete(){
+		this.deleteFlag = true;
+	}
+	
 }
