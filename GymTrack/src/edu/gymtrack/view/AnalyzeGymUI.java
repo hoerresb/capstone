218aff769d154e539d85e9c15c8ee8992a269364
griffinp <griffinp@uwm.edu>
2014-04-27 21:31:13 -0500

    AnalyzeGymUI implemented, take 2

--- GymTrack/src/edu/gymtrack/view/AnalyzeGymUI.java
+++ GymTrack/src/edu/gymtrack/view/AnalyzeGymUI.java
@@ -1,12 +1,36 @@
 package edu.gymtrack.view;
 import java.awt.*;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PiePlot;
+import org.jfree.data.general.DefaultPieDataset;
+import org.jfree.data.general.PieDataset;
+import org.jfree.ui.ApplicationFrame;
+import org.jfree.ui.RefineryUtilities;
+
+import edu.gymtrack.db.Factory;
+import edu.gymtrack.model.Equipment;
+import edu.gymtrack.model.WorkoutLog;
 
 import javax.swing.*;
 
 public class AnalyzeGymUI extends GTUI {
 	private static final long serialVersionUID = 1L;
 	
+	
 	public void createAnalyzeGymUI(GymTrack gym){
+		
+		final Factory factory = new Factory();
+		
+		final PieDataset dataset = createDataset(factory);
+		JFreeChart chart = createChart(dataset);
+		ChartPanel panel = new ChartPanel(chart);
+		
+		
 		gym.getContentPane().removeAll();
 		gym.getContentPane().revalidate();
 		gym.getContentPane().repaint();
@@ -16,7 +40,7 @@ public class AnalyzeGymUI extends GTUI {
 		
 		JPanel topPanel = new JPanel();
 		topPanel.setSize(800, 400);
-		gym.getContentPane().add(topPanel);
+		gym.getContentPane().add(panel);
 		
 		JLabel lblGymtrack = new JLabel("TODO - create AnalyzeGymUI");
 		lblGymtrack.setFont(new Font("Calibri", Font.PLAIN, 30));
@@ -24,12 +48,45 @@ public class AnalyzeGymUI extends GTUI {
 		
 		gym.btnBack_AnalyzeGym = new JButton("Back");
 		gym.btnBack_AnalyzeGym.addActionListener(gym);
-		topPanel.add(gym.btnBack_AnalyzeGym);
+		gym.getContentPane().add(gym.btnBack_AnalyzeGym);
 	}
 
 	@Override
 	public GTUI showUI(GymTrack gym) {
 		createAnalyzeGymUI(gym);
 		return this;
+	}
+	
+	private PieDataset createDataset(Factory factory)
+	{
+		DefaultPieDataset dataset = new DefaultPieDataset();
+		
+		try {
+			ArrayList<Equipment> equipment = factory.getEquipment();
+			for (Equipment current: equipment)
+			{
+				ArrayList<WorkoutLog> logs = factory.getWorkoutLogs(current);
+				dataset.setValue(current.getName(), logs.size());
+			}
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		
+		return dataset;
+	}
+	
+	private JFreeChart createChart(PieDataset dataset)
+	{
+		JFreeChart chart = ChartFactory.createPieChart(
+	            "Equipment Usage",  // chart title
+	            dataset,             // data
+	            true,               // include legend
+	            true,
+	            false
+	        );
+		
+		return chart;
 	}
 }
